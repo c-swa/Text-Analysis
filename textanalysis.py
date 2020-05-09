3695132c06efa0291c520830f8311a8f49cd4980
Christopher Swayne <chris_swa@icloud.com>
2020-05-09 18:46:55 -0500

    textanalysis now can count values
    While I haven't gotten the program to work functionally, I figured I would attempt to get this to work imperatively first, and then build a functional paradigm version of the program.
    
    This version now counts properly.

--- textanalysis.py
+++ textanalysis.py
@@ -2,26 +2,35 @@
 
 # View ReadMe for more project information
 from functools import reduce
+from math import log2
+from collections import defaultdict
+
+FILENAME = "WarAndPeace.txt"
+ALLOWED_CHARACTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+                      'p','q','r','s','t','u','v','w','x','y','z',' ','A','B','C',
+                      'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
+                      'S','T','U','V','W','X','Y','Z'
+                      ]
+
+def filter_characters(str_list):
+    for ch in str_list:
+        if ch in ALLOWED_CHARACTERS:
+            return True
+
+def count_occurences(str_list):
+    char_dict = defaultdict(int)
+
+    for ch in str_list:
+        if ch in ALLOWED_CHARACTERS:
+            char_dict[ch] += 1
+
+    return char_dict
 
 
-def to_upper_case(s):
-    return str(s).upper()
 
+def text_analysis():
+    # Pulls the raw text from file and places it in an array
+    raw_text = str(list(open(FILENAME, 'r').read()))
+    print("Test of count_occurences: " + str(dict(count_occurences(raw_text))))
 
-def print_iterator(it):
-    return reduce()
-
-
-def analysis(line):
-    print(line)
-
-
-def text():
-    filename = "WarAndPeace.txt"
-    file = open(filename, "r")
-    map_iterator = map(analysis, file.readlines())
-
-
-map_iterator = map(to_upper_case, 'abc')
-print(type(map_iterator))
-print_iterator(map_iterator)
+text_analysis()
